be585862a6db8d4f0a7b6c4f824b39e7c9717656
Michael Stringer <stringbean@users.noreply.github.com>
2017-01-06 17:27:36 +0000

    Add KML writer

---
+++ poi4s-kml/src/main/scala/com/pds/poi4s/kml/KmlWriter.scala
@@ -0,0 +1,41 @@
+package com.pds.poi4s.kml
+
+import java.io.OutputStream
+
+import scala.xml.{Elem, PrettyPrinter}
+
+object KmlWriter {
+  private val prettyPrinter = new PrettyPrinter(80, 4)
+
+  def write(kmlFile: KmlFile, os: OutputStream): Unit = {
+    val xml = generateKml(kmlFile)
+    os.write(prettyPrinter.format(xml).getBytes("UTF-8"))
+  }
+
+  private def generateKml(kmlFile: KmlFile): Elem = {
+    <kml xmlns="http://www.opengis.net/kml/2.2">
+      <Document>
+        {
+          kmlFile.placemarks map { placemark =>
+            <Placemark>
+              {placemark.name.map(n => <name>{n}</name>).orNull}
+              {placemark.description.map(p => <description>{p}</description>).orNull}
+              <Point>
+                <coordinates>
+                  {
+                    placemark.elevation match {
+                      case Some(e) =>
+                        s"${placemark.lon},${placemark.lat},$e"
+                      case None =>
+                        s"${placemark.lon},${placemark.lat}"
+                    }
+                  }
+                </coordinates>
+              </Point>
+            </Placemark>
+          }
+        }
+      </Document>
+    </kml>
+  }
+}
---
+++ poi4s-kml/src/test/scala/com/pds/poi4s/kml/KmlWriterSpec.scala
@@ -0,0 +1,69 @@
+package com.pds.poi4s.kml
+
+import java.io.{ByteArrayOutputStream, StringReader}
+
+import com.pds.poi4s.model.Waypoint
+import org.scalatest.StreamlinedXmlEquality._
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.xml.{Elem, XML}
+
+class KmlWriterSpec extends FlatSpec with Matchers {
+
+  "KmlWriter" should "generate an empty KML file" in {
+    val kml = KmlFile(Nil)
+    val xml = generateFile(kml)
+
+    val expected = <kml xmlns="http://www.opengis.net/kml/2.2">
+      <Document/>
+    </kml>
+
+    xml should ===(expected)
+  }
+
+  it should "generate KML file with waypoints" in {
+    val kml = KmlFile(
+      Seq(
+        Waypoint(
+          51.4994794d,
+          -0.12480919999995876d,
+          elevation = Some(2.134d),
+          name = Some("Palace of Westminster"),
+          description = Some("Seat of the UK parliament")
+        ),
+        Waypoint(
+          51.504722,
+          -0.1375,
+          name = Some("St James's Palace")
+        )
+      )
+    )
+
+    val xml = generateFile(kml)
+    val expected = <kml xmlns="http://www.opengis.net/kml/2.2">
+      <Document>
+        <Placemark>
+          <name>Palace of Westminster</name>
+          <description>Seat of the UK parliament</description>
+          <Point>
+            <coordinates>-0.12480919999995876,51.4994794,2.134</coordinates>
+          </Point>
+        </Placemark>
+        <Placemark>
+          <name>St James's Palace</name>
+          <Point>
+            <coordinates>-0.1375,51.504722</coordinates>
+          </Point>
+        </Placemark>
+      </Document>
+    </kml>
+
+    xml should ===(expected)
+  }
+
+  private def generateFile(kmlFile: KmlFile): Elem = {
+    val baos = new ByteArrayOutputStream()
+    KmlWriter.write(kmlFile, baos)
+    XML.load(new StringReader(baos.toString("UTF-8")))
+  }
+}
